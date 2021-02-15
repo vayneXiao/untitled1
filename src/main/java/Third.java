faa8d7655204cae42822026e255200d37609828e
vaynexiao <576583212@qq.com>
2021-02-15 13:01:06 +0800

    Third.java

---
+++ src/main/java/Third.java
@@ -0,0 +1,25 @@
+public class Third {
+    private Long uid;
+    private String username;
+
+    public Third(Long uid, String username) {
+        this.uid = uid;
+        this.username = username;
+    }
+
+    public Long getUid() {
+        return uid;
+    }
+
+    public void setUid(Long uid) {
+        this.uid = uid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
